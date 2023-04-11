7b041380bd4bb06d40d6b0ce1f04a3120a7520ed
Bolaji06 <joshspencer0603@gmail.com>
2023-04-11 06:58:56 -1200

    first commit of screentym

---
+++ data-request.js
@@ -0,0 +1,61 @@
+
+// API KEY --> b6d2f70b74eb483aeb5bb0ee43a82e53
+// URL --> https://api.themoviedb.org/3/movie/550?api_key=b6d2f70b74eb483aeb5bb0ee43a82e53
+
+const heroBackDrop = document.querySelector('.hero-bg');
+const movieTitle = document.querySelector('.movie-title');
+const movieDate = document.querySelector('.rec-details-year');
+const movieGenre = document.querySelector('.rec-details-genre');
+const gridItems = document.querySelectorAll('.item');
+
+const movieGridContainer = document.querySelector('.rec-movie-grid');
+const recMovieTitle = document.querySelector('.movie-title')
+
+
+const API_KEY = 'b6d2f70b74eb483aeb5bb0ee43a82e53'
+async function fetchData(){
+    try{
+        const response = await fetch(`
+        https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+        const data = await response.json();
+        const firstNine = data.results.slice(1, 10); // Get the first nine movie items from list
+        console.log(firstNine)
+        
+
+      
+
+        
+            firstNine.forEach((movie) => {
+                
+                const objList = Object.values(movie);
+                //console.log(objList);
+               
+                    const imgEl = document.createElement('img');
+                    imgEl.setAttribute('class', 'item')
+                    imgEl.src = `https://image.tmdb.org/t/p/w200/${objList[1]}`
+                    movieGridContainer.appendChild(imgEl)
+
+                    imgEl.addEventListener('click', ()=>{
+                        recMovieTitle.textContent = objList[5];
+                        heroBackDrop.src = `https://image.tmdb.org/t/p/w500${objList[1]}`
+                    
+                    })
+            })
+
+
+        const poster = data.results[0];
+        const {title, release_date, genre_ids, backdrop_path} = poster
+        //heroBackDrop.src = `https://image.tmdb.org/t/p/w500/${backdrop_path}`;
+        movieTitle.textContent = title;
+        movieDate.textContent = release_date;
+        movieGenre.textContent = genre_ids[0];
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+fetchData()
+
+// fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
+//     .then(response => response.json())
+//     .then(data => data)
---
+++ index.js
@@ -0,0 +1,78 @@
+
+
+const menuEl = document.querySelector('.menu-bar');
+const logo = document.querySelector('.logo');
+const serachWrapper = document.querySelector('.search-wrapper');
+const searchInput = document.querySelector('.movie-search-input');
+const closeEl = document.querySelector('.close-btn');
+//const logo = document.querySelector('.logo')
+const recommendSlider = document.querySelector('.item-list');
+
+const sideNav = document.querySelector('.sidenav');
+const main = document.querySelector('.main');
+
+const recGrid = document.querySelector('.rec-movie-grid');
+
+let isDown = false;
+let startX, scrollLeft;
+
+
+
+function openNav(){
+    sideNav.style.width = '300px';
+    main.style.marginLeft = '250px'
+    document.body.backgroundColor = 'black';
+
+}
+function closeNav(){
+    sideNav.style.width = '0';
+    main.style.marginLeft = '0'
+    document.body.backgroundColor = 'red';
+}
+
+menuEl.addEventListener('click', openNav);
+closeEl.addEventListener('click', closeNav)
+
+if (window.innerWidth <= 600){
+    searchInput.addEventListener('focus', ()=>{
+        logo.classList.add('remove-logo')
+    })
+}
+
+// The recommended slider movie list
+recommendSlider.addEventListener('mousedown', (e)=>{
+    isDown = true;
+    recommendSlider.classList.add('active');
+    startX = e.pageX - recommendSlider.offsetLeft;
+    scrollLeft = recommendSlider.scrollLeft;
+});
+
+recommendSlider.addEventListener('mouseleave', ()=>{
+    isDown = false;
+    recommendSlider.classList.remove('active');
+});
+
+recommendSlider.addEventListener('mouseup', ()=>{
+    isDown = false;
+    recommendSlider.classList.remove('active')
+});
+
+recommendSlider.addEventListener('mousemove', (e)=>{
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - recommendSlider.offsetLeft;
+    const walk = (x - startX) * 4;  // You can change the multiplied value to change slider speed
+    recommendSlider.scrollLeft = scrollLeft - walk;  
+    
+});
+
+window.addEventListener('mousemove', (e)=>{
+    //console.log(e.pageX, e.pageY)
+})
+
+recGrid.addEventListener('dragstart', (e)=>{
+    e.preventDefault();
+})
+recGrid.addEventListener('drop', (e)=>{
+    e.preventDefault()
+})
